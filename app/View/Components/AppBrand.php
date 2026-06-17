5eaa81a9a8d834dd6a4ccd8f67a01ffcd0173e4e
Michael Grunewalder <michael.grunewalder@eopus.de>
2026-06-17 08:48:25 +0200

    Default MaryUI

--- app/View/Components/AppBrand.php
+++ app/View/Components/AppBrand.php
@@ -16,23 +16,26 @@ class AppBrand extends Component
         //
     }
 
+    /**
+     * Get the view / contents that represent the component.
+     */
     public function render(): View|Closure|string
     {
         return <<<'HTML'
                 <a href="/" wire:navigate>
                     <!-- Hidden when collapsed -->
                     <div {{ $attributes->class(["hidden-when-collapsed"]) }}>
-                        <div class="flex flex-col items-center gap-2 w-fit">
-                            <img src="/img/bearny_codes.png" /><br/>
-                            <span class="font-bold text-2xl me-3 bg-gradient-to-r from-purple-500 to-pink-300 bg-clip-text text-transparent" style="display:none" >
-                                {{config('app.name', 'Laravel')}}
+                        <div class="flex items-center gap-2 w-fit">
+                            <x-icon name="o-cube" class="w-6 -mb-1.5 text-purple-500" />
+                            <span class="font-bold text-3xl me-3 bg-gradient-to-r from-purple-500 to-pink-300 bg-clip-text text-transparent ">
+                                app
                             </span>
                         </div>
                     </div>
 
                     <!-- Display when collapsed -->
                     <div class="display-when-collapsed hidden mx-5 mt-5 mb-1 h-[28px]">
-                        <x-icon name="o-rocket-launch" class="w-6 -mb-1.5 text-purple-500" />
+                        <x-icon name="s-cube" class="w-6 -mb-1.5 text-purple-500" />
                     </div>
                 </a>
             HTML;
